5e2031966f396d421502e3b223dba9c21d60b472
Peyton Doyle <p6doyle@gmail.com>
2019-07-21 14:19:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,13 +23,12 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
-    source_array.each do |item|
-      
-end
+    if source_array.each do |item| = false
+      return false
+    end
+    return true
 end
 
-    
-end
 
 
 def reduce_to_any_true(source_array)
